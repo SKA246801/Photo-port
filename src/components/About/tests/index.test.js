46450174e50ea326a2241d5472629b7313381247
Sebastian <sebastianalvarado246801@gmail.com>
2021-07-28 10:36:26 -0500

    added Nav tests

--- src/components/About/tests/index.test.js
+++ src/components/About/tests/index.test.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import About from '..'
-const { asFragment } = render(<About />)
 
 afterEach(cleanup)
 
@@ -12,6 +11,8 @@ describe('About compnent', () => {
     })
 
     it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About />)
+        
         expect(asFragment()).toMatchSnapshot()
     })
 })
